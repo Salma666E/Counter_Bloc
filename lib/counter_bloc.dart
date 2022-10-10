36290f3ad9957ee3858b8a9425af30c6ad7549c9
Salma666E <smsmacs666@gmail.com>
2022-10-10 10:55:29 +0200

    first commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter_cubit.dart';
+
+/// {@template counter_bloc}
+/// A simple [Bloc] that manages an `int` as its state.
+/// {@endtemplate}
+class CounterBloc extends Bloc<CounterEvent, int> {
+  /// {@macro counter_bloc}
+  CounterBloc() : super(0) {
+    on<CounterIncrementPressed>((event, emit) => emit(state + 1));
+    on<CounterDecrementPressed>((event, emit) => emit(state - 1));
+  }
+}
